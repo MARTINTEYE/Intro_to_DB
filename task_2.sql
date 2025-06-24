8b0a7bd119cb7846e15dbe30affa297b93c41936
MARTIN TEYE <martinteye93@gmail.com>
2025-06-24 15:04:42 +0000

    fix capitalization of AUTHORS

--- task_2.sql
+++ task_2.sql
@@ -1,44 +1,44 @@
 -- USE THE DATABASE
 USE alx_book_store;
 
--- CREATE AUTHORS TABLE
-CREATE TABLE IF NOT EXISTS authors (
+-- CREATE Authors TABLE
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
--- CREATE BOOKS TABLE
-CREATE TABLE IF NOT EXISTS books (
+-- CREATE Books TABLE
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
     price DOUBLE NOT NULL,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- CREATE CUSTOMERS TABLE
-CREATE TABLE IF NOT EXISTS customers (
+-- CREATE Customers TABLE
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
     address TEXT
 );
 
--- CREATE ORDERS TABLE
-CREATE TABLE IF NOT EXISTS orders (
+-- CREATE Orders TABLE
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- CREATE ORDER_DETAILS TABLE
-CREATE TABLE IF NOT EXISTS order_details (
+-- CREATE Order_Details TABLE
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
